7d850b30c4fa57fe96cbd234fbeb41d89e33b850
OpenDeer <yoursnb@gmail.com>
2026-09-23 10:47:50 +0800

    feat: wire file transfer progress events

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@fontsource-variable/noto-sans-sc";
 import App from "./App";
 import "./styles.css";
+import "./features/files/transfer-progress";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
